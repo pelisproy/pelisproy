74c2accc28e585c0dde3e5ef9ec64d63e7d5b1ac
JorgeAbejer <abejergarciajorge@gmail.com>
2015-11-10 21:51:37 +0100

    Actualización del fichero crearPeli.js y creación de ficheros resultadosBusqueda.css y .js

---
+++ ProyectoFinal/assets/js/resultadosBusqueda.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.glyphicon-menu-hamburger').on('click', function() {
+		if($('.menu').css('display') == 'none') {
+    		$('.menu').css('display', 'block');
+    	} else {
+    		$('.menu').css('display', 'none');
+    	}
+    });
+	
+});
